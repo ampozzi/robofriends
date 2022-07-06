95a193f3060a56b4fd8f9d2d303a030ff667a62e
apozzi <axelpozzi@gmail.com>
2022-07-06 16:00:50 -0500

    changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,6 +12,7 @@ class App extends Component{
             robots:[],
             searchField:''
         };
+        // this.onSearchChange=this.onSearchChange.bind(this);
     }
 
     componentDidMount(){
@@ -23,12 +24,13 @@ class App extends Component{
     onSearchChange=(event)=>{
         this.setState({searchField: event.target.value})
     }
+    
     render(){
         const { robots, searchField} =this.state;
         const filteredRobots=robots.filter(robot=>robot.name.toLowerCase().includes(searchField.toLowerCase()));
         return (
             <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <Sticky id='stickysearch'>
                     <SearchBox searchChange={this.onSearchChange}/>
                 </Sticky>
